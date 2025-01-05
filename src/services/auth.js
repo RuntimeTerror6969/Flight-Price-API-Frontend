011e466a9b524ad12d2b7846b8a33eae1566c6b9
adarsh <aadarsh.22.2003@gmail.com>
2025-01-05 20:51:56 +0530

    Linking Backend

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
-const API_URL = "https://flight-price-api-backend.vercel.app/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+// const API_URL = "https://flight-price-api-backend.vercel.app/api";
 
 export const register = async (email, password) => {
   const response = await axios.post(`${API_URL}/auth/register`, {
