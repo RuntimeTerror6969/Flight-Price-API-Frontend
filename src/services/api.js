f007672b5210804f81520c5f89272240c8723612
adarsh <aadarsh.22.2003@gmail.com>
2025-01-05 14:24:24 +0530

    Linking Backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
-
+// const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = "https://flight-price-api-backend.vercel.app/api";
 export const searchFlights = async (searchData) => {
   console.log("Sending search data:", searchData);
   try {
